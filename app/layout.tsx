c65ed197f0c14c6dbbf3118a6cb57d5e2c67f06a
Biruk Hamid Rebsom (Nokia) <biruk.hamid_rebsom@nokia.com>
2025-04-11 17:52:23 +0300

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,13 @@ export const metadata: Metadata = {
   },
   description: "Find and explore restaurants and their delicious menus",
   metadataBase: new URL("https://etmenu.vercel.app"),
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png", sizes: "32x32" },
+    ],
+    apple: { url: "/apple-icon.png", type: "image/png", sizes: "180x180" },
+  },
   openGraph: {
     type: "website",
     locale: "en_US",
